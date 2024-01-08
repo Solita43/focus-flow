64e64ee5d872aa03a522daf6e3cddfac44210342
Melinda Cortez <meli.matrix@outlook.com>
2024-01-07 21:52:36 -0800

    feat:Create and test logout endpoint

--- accounts/views.py
+++ accounts/views.py
@@ -4,14 +4,24 @@ from rest_framework.response import Response
 from .serializers import SignUpSerializer
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import ensure_csrf_cookie, csrf_protect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from .models import UserAccount
 
-@method_decorator(csrf_protect, name='dispatch')
+
+@method_decorator(csrf_protect, name="dispatch")
 class SignUpView(generics.CreateAPIView):
     """
-        Validate data and create a new user account if valid.
+    Validate data and create a new user account if valid.
+
+    Expected Request Body:
+    {
+        "email": "example@example.com,
+        "username": "exampleuser",
+        "password": "secure_password",
+        "re_password": "secure_password"
+    }
     """
+
     serializer_class = SignUpSerializer
     permission_classes = [permissions.AllowAny]
 
@@ -20,41 +30,58 @@ class SignUpView(generics.CreateAPIView):
 
         if serializer.is_valid():
             user = UserAccount.objects.create_user(**serializer.validated_data)
+            login(request, user)
 
             response_data = {
-                "message": "User Created Successfully", "data": serializer.data
+                "message": "User Created Successfully",
+                "data": user.to_dict(),
             }
 
-            return Response(data=response_data, status=200)
+            return Response(data=response_data, status=201)
 
         error_data = {"error": serializer.errors}
         return Response(data=error_data, status=400)
 
-@method_decorator(csrf_protect, name='dispatch')
+
+@method_decorator(csrf_protect, name="dispatch")
 class LoginView(APIView):
     """
-        Authenticate the user and log them in.
+    Authenticate the user and log them in.
+
+    Expected Request Body:
+    {
+        "email": "example@example.com",
+        "password": "secure_password"
+    }
     """
+
     permission_classes = [permissions.AllowAny]
 
     def post(self, request, *args, **kwargs):
-        email = request.data['email']
-        password = request.data['password']
+        email = request.data["email"]
+        password = request.data["password"]
 
         user = authenticate(email=email, password=password)
         if user is not None:
             login(request, user)
-            return Response({'message': 'Login successful'}, status=200)
+            return Response({"message": "Login successful!"}, status=200)
         else:
-            return Response({'error': 'Invalid credentials'}, status=401)
-        
-@method_decorator
+            return Response({"error": "Invalid credentials"}, status=401)
 
-@method_decorator(ensure_csrf_cookie, name='dispatch')
+
+class LogoutView(APIView):
+    def post(self, request, *args, **kwargs):
+        logout(request)
+        return Response({"message": "User successfully logged out!"}, 200)
+
+
+@method_decorator(ensure_csrf_cookie, name="dispatch")
 class GetCSRFToken(APIView):
     """
-        Create and set csrf token.
+    Create and set csrf token.
     """
+
     permission_classes = [permissions.AllowAny]
+
     def get(self, request, *args, **kwargs):
         return Response({"message": "CSRF successfully set!"}, status=200)
