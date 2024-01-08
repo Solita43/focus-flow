9a1cd5b011ed72b94377b791e18e24bdb3e84a5a
Melinda Cortez <meli.matrix@outlook.com>
2024-01-07 20:17:23 -0800

    feat: Create and test login endpoint

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("register", views.SignUpView.as_view()),
-    path("csrf_cookie", views.GetCSRFToken.as_view())
+    path("login", views.LoginView.as_view()),
+    path("csrf_cookie", views.GetCSRFToken.as_view()),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,8 @@ from rest_framework.response import Response
 from .serializers import SignUpSerializer
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import ensure_csrf_cookie, csrf_protect
-from django.middleware.csrf import get_token
+from django.contrib.auth import authenticate, login
+from .models import UserAccount
 
 @method_decorator(csrf_protect, name='dispatch')
 class SignUpView(generics.CreateAPIView):
@@ -17,9 +18,8 @@ class SignUpView(generics.CreateAPIView):
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
 
-
         if serializer.is_valid():
-            serializer.save()
+            user = UserAccount.objects.create_user(**serializer.validated_data)
 
             response_data = {
                 "message": "User Created Successfully", "data": serializer.data
@@ -30,8 +30,29 @@ class SignUpView(generics.CreateAPIView):
         error_data = {"error": serializer.errors}
         return Response(data=error_data, status=400)
 
+@method_decorator(csrf_protect, name='dispatch')
+class LoginView(APIView):
+    """
+        Authenticate the user and log them in.
+    """
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        username = request.data['username']
+        password = request.data['password']
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return Response({'message': 'Login successful'}, status=200)
+        else:
+            return Response({'error': 'Invalid credentials'}, status=401)
+
 @method_decorator(ensure_csrf_cookie, name='dispatch')
 class GetCSRFToken(APIView):
+    """
+        Create and set csrf token.
+    """
     permission_classes = [permissions.AllowAny]
     def get(self, request, *args, **kwargs):
         return Response({"message": "CSRF successfully set!"}, status=200)
